29fdf29a38c32f5785363eca48e815a4d89455d8
Gowa2017 <33367355+Gowa2017@users.noreply.github.com>
2025-11-03 12:36:46 +0800

    feat: show plain private key only in plain mode

--- cmd/root.go
+++ cmd/root.go
@@ -13,6 +13,7 @@ import (
 )
 
 var path string
+var plain bool
 
 var rootCmd = GetRootCommand()
 
@@ -31,6 +32,7 @@ func GetRootCommand() *cobra.Command {
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&path, "config", "c", "", "配置文件路径")
+	rootCmd.PersistentFlags().BoolVarP(&plain, "plain", "p", false, "是否解析密文信息")
 	rootCmd.AddCommand(GetTomlCommand())
 	rootCmd.AddCommand(SetTomlCommand())
 	rootCmd.AddCommand(ListTomlCommand())
@@ -102,7 +104,11 @@ func printAConfigure(key string, v any) {
 		for _, k := range keys {
 			v = treeMap[k]
 			if s, ok := v.(string); ok {
-				fmt.Printf("%-*s = %s\n", maxKeyLength, k, s)
+				if k == "private_key" && !plain {
+					fmt.Printf("%-*s = %s\n", maxKeyLength, k, "********************")
+				} else {
+					fmt.Printf("%-*s = %s\n", maxKeyLength, k, s)
+				}
 			} else if m, ok := v.(map[string]any); ok {
 				fmt.Printf("%s:\n", k)
 				for kk, vv := range m {
